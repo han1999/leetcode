2fb5465832f2cac0fc5a9bfeb71276c2a69e59d1
Han Xiao <1043599451@qq.com>
2022-08-11 23:15:53 +0800

    Finish 47-50

---
+++ quickSort.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int partition(vector<int> &nums, int l, int r){
+	int pivot=l-1;
+	for (int i=l; i<r; i++){
+		if (nums[i]<nums[r]){
+			swap(nums[i], nums[++pivot]);
+		}
+	}
+	swap(nums[r], nums[++pivot]);
+	return pivot;	
+}
+
+void quickSort(vector<int> &nums, int l, int r){
+	if (l>=r) return;
+	int pivot=partition(nums, l, r);
+	quickSort(nums, l, pivot-1);
+	quickSort(nums, pivot+1, r);
+}
+int main(){
+	int n;
+	cin>>n;
+	vector<int> nums;
+	for (int i=0; i<n; i++){
+		int num;
+		cin>>num;
+		nums.emplace_back(num);
+	}
+	
+	quickSort(nums, 0, nums.size()-1);
+	for (const auto &num:nums){
+		cout<<num<<" ";
+	}
+}
